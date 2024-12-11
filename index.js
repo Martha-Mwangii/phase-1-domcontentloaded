9dd6513f16decfaa8df7d91559b88c1051da8fec
Martha Wangui Mwangi <marthawm7252@gmail.com>
2024-12-11 20:44:33 +0300

    phase-1-domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // const elem = document.getElementById(text)//displays the initial text in the index.html file
+    const element = document.querySelector("#text")//replaces the text in the index.html
+    element.textContent = "This is really cool!"
+    console.log(element);
+  });
